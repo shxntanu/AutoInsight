cc80ab126c644aa5e3ba351e393195c57b36e8e9
Shantanu <shantanuwable2003@gmail.com>
2023-04-07 16:29:56 +0530

    Added a testing screen to view all screens

---
+++ AutoInsight/lib/screens/test_screen/test_screen.dart
@@ -0,0 +1,62 @@
+// Starting screen to view different screens
+
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pushNamed(context, '/landing');
+              },
+              child: const Text('Landing'),
+              ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: const Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/register');
+              },
+              child: const Text('Register'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              child: const Text('Home'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/og');
+              },
+              child: const Text('OG Landing Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/new');
+              },
+              child: const Text('New Landing Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/pullup');
+              },
+              child: const Text('Pull Up Landing Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
