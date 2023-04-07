c0db3e38ee9cc37a4bd94431a92ffe1b5f287edf
Shantanu <shantanuwable2003@gmail.com>
2023-04-07 16:27:54 +0530

    Added testing screen & all other screens as routes

--- AutoInsight/lib/main.dart
+++ AutoInsight/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:autoinsight/screens/onboarding/new_landing_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/test_screen/test_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/landing_screen.dart';
 import 'screens/home_screen.dart';
-import 'screens/temp_screen.dart';
+import 'screens/PullUpLandingScreen.dart';
+import 'screens/og_landing_page.dart';
 import 'screens/onboarding/new_landing_screen.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 
@@ -18,18 +20,17 @@ class AutoInsight extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'San Francisco'),
-      initialRoute: '/temp',
+      initialRoute: '/',
       routes: {
-        '/': (context) => LandingScreen(),
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegistrationScreen(),
-        '/home': (context) => HomeScreen(),
-        '/temp': (context) => NewLandingScreen(),
+        '/' : (context) => const TestScreen(),                  // Starting screen to view different screens
+        '/landing': (context) => const LandingScreen(),         // Circular Design landing screen
+        '/login': (context) => const LoginScreen(),             // Circular Design login screen
+        '/register': (context) => const RegistrationScreen(),   // Circular Design Registration screen
+        '/home': (context) => HomeScreen(),                     // Unfinished home page
+        '/new': (context) => NewLandingScreen(),                // New Landing screen (from the sample app) that we're building
+        '/og' : (context) => const OGEntryPage(),               // Original landing screen
+        '/pullup' : (context) => const PullUpLandingScreen(),   // Pull Up Landing Screen
       },
     );
   }
 }
-
-
-// Hello there
-// byoyoyouijxskxhwkehxlwejlxwehxwhelxjcwlexlwe
