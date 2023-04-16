8711e04230b1e3272a87ab2e0e3d57c9751f00d8
Shantanu <shantanuwable2003@gmail.com>
2023-04-16 22:45:46 +0530

    Initialised Firebase

--- autoinsight/lib/main.dart
+++ autoinsight/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../screens/onboarding/signin_page.dart';
+import '../firebase_options.dart';
 
 void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
