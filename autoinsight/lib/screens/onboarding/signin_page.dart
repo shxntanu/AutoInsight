ee1c68f1e1fbce0c1bb90be62ba657ee4c2d5125
Shantanu <shantanuwable2003@gmail.com>
2023-04-16 22:46:27 +0530

    Misc

--- autoinsight/lib/screens/onboarding/signin_page.dart
+++ autoinsight/lib/screens/onboarding/signin_page.dart
@@ -1,8 +1,10 @@
 import 'package:autoinsight/screens/onboarding/components/signin_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
