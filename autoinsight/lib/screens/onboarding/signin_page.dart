15a88aa3d4a88f021036908d56c7f9965f0ae07f
Shantanu <shantanuwable2003@gmail.com>
2023-04-16 21:04:47 +0530

    Added sign-in page and its components

---
+++ autoinsight/lib/screens/onboarding/signin_page.dart
@@ -0,0 +1,88 @@
+import 'package:autoinsight/screens/onboarding/signin_textfield.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            Color.fromARGB(255, 240, 87, 87),
+            Color.fromARGB(255, 252, 125, 108),
+            Color.fromARGB(255, 248, 154, 114),
+            Color.fromARGB(255, 246, 187, 169),
+            Color.fromARGB(255, 239, 220, 220)
+          ])),
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            foregroundColor: const Color(0xFF03045E),
+          ),
+          backgroundColor: const Color(0x00000000),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(50.0),
+                    child: Card(
+                      margin: EdgeInsets.only(top: 20, right: 30, left: 30),
+                      color: Colors.white,
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                            color: Color(0xFF03045E),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.white,
+                    child: Column(
+                      children: const [
+                        Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: SignIn_TextField(
+                              icon: Icons.email,
+                              hintText: 'enter email',
+                              textType: TextInputType.emailAddress,
+                              obscureText: false,
+                            )),
+                        Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: SignIn_TextField(
+                            icon: Icons.key,
+                            hintText: 'enter password',
+                            textType: TextInputType.visiblePassword,
+                            obscureText: true,
+                          ),
+                        ),
+                        Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: SignIn_TextField(
+                              icon: Icons.key,
+                              hintText: 'confirm password',
+                              textType: TextInputType.visiblePassword,
+                              obscureText: true,
+                            ))
+                      ],
+                    ),
+                  )
+                ]),
+          )),
+    );
+  }
+}
+
+// FF7171
+// FA6E00
+// FFB342
---
+++ autoinsight/lib/screens/onboarding/signin_textfield.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SignIn_TextField extends StatelessWidget {
+
+  const SignIn_TextField(
+        {
+          required this.icon,
+          required this.hintText,
+          required this.textType,
+          required this.obscureText
+        }
+      );
+
+  final IconData icon;
+  final String hintText;
+  final TextInputType textType;
+  final bool obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //margin: EdgeInsets.only(top: 20),
+      color: const Color(0xFFF2F2F2),
+      child: TextField(
+        obscureText: obscureText,
+        keyboardType: textType,
+        decoration: InputDecoration(
+            icon: Icon(
+              icon,
+              color: const Color(0xFF03045E),
+            ),
+            hintText: ' $hintText',
+            hintStyle: const TextStyle(color: Colors.grey)),
+      ),
+    );
+  }
+}
