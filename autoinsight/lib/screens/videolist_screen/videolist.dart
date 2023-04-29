280791eab71794f8eda8bfe4f4d6e83a046f6e21
Shantanu <shantanuwable2003@gmail.com>
2023-04-29 23:57:28 +0530

    Added a screen to show videos

---
+++ autoinsight/lib/screens/videolist_screen/videolist.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class VideoList extends StatefulWidget {
+  const VideoList({super.key});
+
+  @override
+  State<VideoList> createState() => _VideoListState();
+}
+
+class _VideoListState extends State<VideoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video List'),
+      ),
+      body: 
+      );
+  }
+}
