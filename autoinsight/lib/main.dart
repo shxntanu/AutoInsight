0326e1b3100d45f3a49396821d26fb1761bf4219
Shantanu <shantanuwable2003@gmail.com>
2023-04-29 23:56:22 +0530

    Added homescreen and other pages

--- autoinsight/lib/main.dart
+++ autoinsight/lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:autoinsight/screens/navigationbarScreens/profile_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'screens/onboarding/signup_page.dart';
+import '../screens/onboarding/signup_page.dart';
 import '../screens/camera_page/camera_page.dart';
-import '../screens/camera_page/video_page.dart';
 import '../screens/car_screen/car_screen.dart';
+import '../screens/dashboard/homescreen.dart';
+import '../screens/dashboard/components/newtrip.dart';
 
 import '../firebase_options.dart';
 
@@ -26,11 +29,14 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'AutoInsight',
       // home: SignUpPage(),
-      home: const CarScreen(),
+      home: HomeScreen(),
       getPages: [
         GetPage(name: '/', page: () => SignUpPage()),
         GetPage(name: '/camera', page: () => const CameraPage()),
         GetPage(name: '/car', page: () => const CarScreen()),
+        GetPage(name: '/home', page: () => HomeScreen()),
+        GetPage(name: '/profile',  page: () => const Profile_Page()),
+        GetPage(name: '/newtrip', page: () => const New_trip()),
       ],
     );
   }
