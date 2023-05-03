71fef932acdba0b701d253e66dee568efe3d9a59
Ridhima Sinare <ridhisinare@gmail.com>
2023-05-03 12:50:53 +0530

    Changed layout and added textfields

--- AutoInsight/lib/screens/dashboard/components/new_trip.dart
+++ AutoInsight/lib/screens/dashboard/components/new_trip.dart
@@ -1,29 +1,81 @@
 import 'package:flutter/material.dart';
 
 class New_trip extends StatelessWidget {
-  const New_trip({super.key});
+  String start;
+  String end;
+  String distance;
+  final Function addtripCallback;
+  New_trip(this.addtripCallback,
+      {required this.start, required this.end, required this.distance});
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: Colors.white,
-      title: Text('Create New Trip'),
-      content: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(hintText: 'current location'),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: 'destinaton'),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('Create'))
-        ],
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'New Trip',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.blueGrey.shade900,
+                  fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              autofocus: true,
+              cursorColor: Colors.blueGrey,
+              onChanged: (newText) {
+                start = newText;
+              },
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  hintText: 'enter starting location',
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade900))),
+            ),
+            TextField(
+              autofocus: true,
+              cursorColor: Colors.blueGrey,
+              onChanged: (newText) {
+                end = newText;
+              },
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  hintText: 'enter destination',
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade900))),
+            ),
+            TextField(
+              autofocus: true,
+              cursorColor: Colors.blueGrey,
+              onChanged: (newText) {
+                distance = newText;
+              },
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  hintText: 'enter distance',
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade900))),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                addtripCallback(start, end, distance);
+              },
+              child: Text('Create Trip'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey.shade900),
+            )
+          ],
+        ),
       ),
     );
   }
