d9e002afa3e534e3907d2329fadee0c1a8ec401d
Shantanu <shantanuwable2003@gmail.com>
2023-05-04 15:41:30 +0530

    Removed commented code

--- autoinsight/lib/screens/onboarding/login_page.dart
+++ autoinsight/lib/screens/onboarding/login_page.dart
@@ -98,28 +98,19 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     onPressed: () async {
-                      // late final user;
-                      // user = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      //   email: _ctr1.text,
-                      //   password: _ctr2.text,
-                      // );
-
-                      // if(user != null) {
-                      //   Get.to('/camera');
-                      // }
-
+                        
                         try {
                           final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
                             email: _ctr1.text,
                             password: _ctr2.text
                           );
                         } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          print('No user found for that email.');
-                        } else if (e.code == 'wrong-password') {
-                          print('Wrong password provided for that user.');
-                        }
-                      }
+                          if (e.code == 'user-not-found') {
+                            print('No user found for that email.');
+                          } else if (e.code == 'wrong-password') {
+                            print('Wrong password provided for that user.');
+                          }
+                        } 
                     }, 
                     child: const Text(
                       "Sign Up",
