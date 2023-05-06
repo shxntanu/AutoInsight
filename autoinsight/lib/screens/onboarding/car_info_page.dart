d47270680681f7ea3bbf6a68d3f76722b41e978f
Shantanu <shantanuwable2003@gmail.com>
2023-05-06 13:08:57 +0530

    Added SingleChildScrollView

--- autoinsight/lib/screens/onboarding/car_info_page.dart
+++ autoinsight/lib/screens/onboarding/car_info_page.dart
@@ -23,120 +23,126 @@ class carInfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
             Color.fromARGB(255, 240, 87, 87),
             Color.fromARGB(255, 252, 125, 108),
             Color.fromARGB(255, 248, 154, 114),
             Color.fromARGB(255, 246, 187, 169),
             Color.fromARGB(255, 239, 220, 220)
-          ])),
+          ]
+        ),
+      ),
       child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.white,
             foregroundColor: const Color(0xFF03045E),
           ),
           backgroundColor: const Color(0x00000000),
-          body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(50.0),
-                    child: Card(
-                      margin: EdgeInsets.only(top: 20, right: 30, left: 30),
-                      color: Colors.white,
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Color(0xFF03045E),
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(50.0),
+                  child: Card(
+                    margin: EdgeInsets.only(top: 20, right: 30, left: 30),
+                    color: Colors.white,
+                    child: Text(
+                      'Car Info',
+                      style: TextStyle(
+                          color: Color(0xFF03045E),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  Card(
-                    color: Colors.white,
-                    child: Column(
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: SignIn_TextField(
-                              controller: _name,
-                              icon: Icons.email,
-                              hintText: 'Name',
-                              textType: TextInputType.name,
-                              obscureText: false,
-                            )),
-                        Padding(
+                ),
+                Card(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Padding(
                           padding: const EdgeInsets.all(15.0),
                           child: SignIn_TextField(
-                            controller: _year,
-                            icon: Icons.key,
-                            hintText: 'Phone Number',
-                            textType: TextInputType.phone,
-                            obscureText: true,
-                          ),
+                            controller: _name,
+                            icon: Icons.email,
+                            hintText: 'Car Name',
+                            textType: TextInputType.name,
+                            obscureText: false,
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: SignIn_TextField(
+                          controller: _year,
+                          icon: Icons.key,
+                          hintText: 'Licence Plate',
+                          textType: TextInputType.phone,
+                          obscureText: false,
                         ),
-                        Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: SignIn_TextField(
-                              controller: _licencePlate,
-                              icon: Icons.key,
-                              hintText: 'Address',
-                              textType: TextInputType.streetAddress,
-                              obscureText: true,
-                            ))
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white, 
-                      backgroundColor: const Color.fromARGB(255, 240, 87, 87),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32.0),
                       ),
-                    ),
-                    onPressed: () async {
-                        
-                        final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-                        prefs.setString('carName', _name.text);
-                        prefs.setString('carYear', _year.text);
-                        prefs.setString('licencePlate', _licencePlate.text);
-                      
-                        _backendCtr.createUser(
-                          prefs.getString("email") ?? "null", 
-                          prefs.getString("password") ?? "null", 
-                          prefs.getString("name") ?? "null", 
-                          prefs.getString("phone") ?? "null", 
-                          prefs.getString("address") ?? "null",  
-                          prefs.getString("carName") ?? "null", 
-                          prefs.getString("carYear") ?? "null",
-                          prefs.getString("licencePlate") ?? "null",
-                        );
-
-                        final user = _auth.createUserWithEmailAndPassword(email: prefs.getString("email") ?? "null", password: prefs.getString("password") ?? "null");
-
-                        Get.offAll(HomeScreen());
-                    }, 
-                    child: const Text(
-                      "Sign Up",
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: SignIn_TextField(
+                          controller: _licencePlate,
+                          icon: Icons.key,
+                          hintText: 'Car Year',
+                          textType: TextInputType.streetAddress,
+                          obscureText: false,
+                        )
+                      )
+                    ],
+                  ),
+                ),
+          
+                const SizedBox(
+                  height: 20,
+                ),
+          
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white, 
+                    backgroundColor: const Color.fromARGB(255, 240, 87, 87),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
                     ),
                   ),
-                ]),
-          )),
+                  onPressed: () async {
+           
+                    final SharedPreferences prefs = await SharedPreferences.getInstance();
+          
+                    prefs.setString('carName', _name.text);
+                    prefs.setString('carYear', _year.text);
+                    prefs.setString('licencePlate', _licencePlate.text);
+                  
+                    _backendCtr.createUser(
+                      prefs.getString("email") ?? "null", 
+                      prefs.getString("password") ?? "null", 
+                      prefs.getString("name") ?? "null", 
+                      prefs.getString("phone") ?? "null", 
+                      prefs.getString("address") ?? "null",  
+                      prefs.getString("carName") ?? "null", 
+                      prefs.getString("carYear") ?? "null",
+                      prefs.getString("licencePlate") ?? "null",
+                    );
+          
+                    final user = _auth.createUserWithEmailAndPassword(email: prefs.getString("email") ?? "null", password: prefs.getString("password") ?? "null");
+          
+                    Get.offAll(HomeScreen());
+                  }, 
+                  child: const Text(
+                    "Sign Up",
+                  ),
+                ),
+              ]
+            ),
+                  ),
+          )
+      ),
     );
   }
 }
--- autoinsight/lib/screens/onboarding/signup_page.dart
+++ autoinsight/lib/screens/onboarding/signup_page.dart
@@ -37,102 +37,104 @@ class SignUpPage extends StatelessWidget {
             foregroundColor: const Color(0xFF03045E),
           ),
           backgroundColor: const Color(0x00000000),
-          body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(50.0),
-                    child: Card(
-                      margin: EdgeInsets.only(top: 20, right: 30, left: 30),
-                      color: Colors.white,
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Color(0xFF03045E),
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(50.0),
+                      child: Card(
+                        margin: EdgeInsets.only(top: 20, right: 30, left: 30),
+                        color: Colors.white,
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Color(0xFF03045E),
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     ),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    child: Column(
-                      children: [
-                        Padding(
+                    Card(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: SignIn_TextField(
+                                controller: _ctr1,
+                                icon: Icons.email,
+                                hintText: 'enter email',
+                                textType: TextInputType.emailAddress,
+                                obscureText: false,
+                              )),
+                          Padding(
                             padding: const EdgeInsets.all(15.0),
                             child: SignIn_TextField(
-                              controller: _ctr1,
-                              icon: Icons.email,
-                              hintText: 'enter email',
-                              textType: TextInputType.emailAddress,
-                              obscureText: false,
-                            )),
-                        Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: SignIn_TextField(
-                            controller: _ctr2,
-                            icon: Icons.key,
-                            hintText: 'enter password',
-                            textType: TextInputType.visiblePassword,
-                            obscureText: true,
-                          ),
-                        ),
-                        Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: SignIn_TextField(
-                              controller: _ctrConfirm,
+                              controller: _ctr2,
                               icon: Icons.key,
-                              hintText: 'confirm password',
+                              hintText: 'enter password',
                               textType: TextInputType.visiblePassword,
                               obscureText: true,
-                            ))
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white, 
-                      backgroundColor: const Color.fromARGB(255, 240, 87, 87),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32.0),
+                            ),
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: SignIn_TextField(
+                                controller: _ctrConfirm,
+                                icon: Icons.key,
+                                hintText: 'confirm password',
+                                textType: TextInputType.visiblePassword,
+                                obscureText: true,
+                              ))
+                        ],
                       ),
                     ),
-                    onPressed: () async {
-                      late final user;
-                      if(_ctr2.text == _ctrConfirm.text){
-                        user = _auth.createUserWithEmailAndPassword(
-                          email: _ctr1.text,
-                          password: _ctr2.text
-                        );
-                      }
-
-                      if(user != null) {
-                        // final path = 'users/${_ctr1.text}/profile.png';
-                        // Reference ref = storage.ref().child(path);
-                        
-                        final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-                        prefs.setString('email', _ctr1.text);
-                        prefs.setString('password', _ctr2.text);
-                        prefs.setBool('logged_in', true);
-                      }
-
-                      Get.offAll(npaInfoPage());
-                    }, 
-                    child: const Text(
-                      "Next",
+          
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                ]),
+          
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white, 
+                        backgroundColor: const Color.fromARGB(255, 240, 87, 87),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32.0),
+                        ),
+                      ),
+                      onPressed: () async {
+                        late final user;
+                        if(_ctr2.text == _ctrConfirm.text){
+                          user = _auth.createUserWithEmailAndPassword(
+                            email: _ctr1.text,
+                            password: _ctr2.text
+                          );
+                        }
+          
+                        if(user != null) {
+                          // final path = 'users/${_ctr1.text}/profile.png';
+                          // Reference ref = storage.ref().child(path);
+                          
+                          final SharedPreferences prefs = await SharedPreferences.getInstance();
+          
+                          prefs.setString('email', _ctr1.text);
+                          prefs.setString('password', _ctr2.text);
+                          prefs.setBool('logged_in', true);
+                        }
+          
+                        Get.to(npaInfoPage());
+                      }, 
+                      child: const Text(
+                        "Next",
+                      ),
+                    ),
+                  ]),
+            ),
           )),
     );
   }
