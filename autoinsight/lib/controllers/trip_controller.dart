2b654c08bff04dc5191d057b72957ccbcb471ace
Shantanu <shantanuwable2003@gmail.com>
2023-05-10 04:50:19 +0530

    Added a trip controller

--- autoinsight/lib/controllers/trip_controller.dart
+++ autoinsight/lib/controllers/trip_controller.dart
@@ -1,5 +1,29 @@
 import 'package:get/get.dart';
+import '../models/previoustrip.dart';
 
+var tripList = [
+  PreviousTrip(
+    start: 'katraj',
+    end: 'fc road',
+    distance: 45,
+  ),
+  PreviousTrip(
+    start: 'katraj',
+    end: 'fc road',
+    distance: 45,
+  ),
+].obs;
 class tripController extends GetxController {
 
+  dynamic getTrip(index) => tripList[index];
+  dynamic get getTripList => tripList;
+  int get getTripCount => tripList.length;
+
+  void addTrip(trip) {
+    tripList.add(trip);
+  }
+
+  // void removeTrip(trip) {
+  //   tripList.remove(trip);
+  // }
 }
