16ae1c0c53596cd55f582897c927329226b1133f
Shantanu <shantanuwable2003@gmail.com>
2023-05-10 11:06:56 +0530

    Added the UI for an alerts screen

--- autoinsight/lib/screens/navigationbarScreens/alert_screen.dart
+++ autoinsight/lib/screens/navigationbarScreens/alert_screen.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/alert_card.dart';
+import '../../utils/alertcard_separator.dart';
+
 class Alertscreen extends StatelessWidget {
   const Alertscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Color(0xFFFF6D6D),title: Text('Alerts'),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Alerts"),
-        ],
+      appBar: AppBar(backgroundColor: const Color(0xFFFF7171),title: const Text('Alerts'),),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CardSeparator(label: "TODAY",),
+              const AlertCard(icon: Icons.car_crash_rounded, title: "Car Damage", subtitle: "Possible car damage detected."),
+              const AlertCard(icon: Icons.dangerous, title: "Forceful Opening", subtitle: "Attempt at forcefully opening car door."),
+              const AlertCard(icon: Icons.car_crash_rounded, title: "Stranger Alert", subtitle: "Possible stranger detected."),
+              CardSeparator(label: "WITHIN ONE WEEK"),
+              const AlertCard(icon: Icons.location_off_rounded , title: "Device left behind", subtitle: "Device has possibly been mislocated."),
+              const AlertCard(icon: Icons.check , title: "Good Driving", subtitle: "Great driving today."),
+              CardSeparator(label: "THIS MONTH"),
+              const AlertCard(icon: Icons.minor_crash_rounded , title: "Near miss", subtitle: "You got too close to another vehicle."),
+            ],
+          ),
+        ),
       ),
     );
   }
