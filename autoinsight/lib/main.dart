a799446f52520960c48c926fa0a769e50e6dd05b
Shantanu <shantanuwable2003@gmail.com>
2023-05-13 12:15:40 +0530

    Removed named routes

--- autoinsight/lib/main.dart
+++ autoinsight/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:autoinsight/screens/navigationbarScreens/alert_screen.dart';
 import 'package:autoinsight/screens/navigationbarScreens/profile_page.dart';
+import 'package:autoinsight/screens/onboarding/login_page/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -7,13 +8,13 @@ import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-import '../screens/onboarding/signup_page.dart';
+import 'unused_pages/signup_page.dart';
 import '../screens/camera_page/camera_page.dart';
 import '../screens/car_screen/car_screen.dart';
 import '../screens/dashboard/homescreen.dart';
 import '../screens/dashboard/components/newtrip.dart';
 import '../screens/videolist_screen/list_page.dart';
-import '../screens/onboarding/start_screen.dart';
+import 'unused_pages/start_screen.dart';
 
 import '../firebase_options.dart';
 
@@ -29,7 +30,7 @@ void main() async {
   );
 
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  // await prefs.clear();
+  await prefs.clear();
   loggedin = prefs.getBool("logged_in") ?? false;
   runApp(const MyApp());
 }
@@ -46,16 +47,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'San Francisco',
       ),
       // home: SignUpPage(),
-      home: loggedin ? HomeScreen() : StartScreen(),
-      getPages: [
-        GetPage(name: '/', page: () => SignUpPage()),
-        GetPage(name: '/camera', page: () => const CameraPage()),
-        GetPage(name: '/car', page: () => const CarScreen()),
-        GetPage(name: '/home', page: () => HomeScreen()),
-        GetPage(name: '/newtrip', page: () => New_trip()),
-        GetPage(name: '/videolist', page: () => VideoList()),
-        GetPage(name: '/alerts', page: () => const Alertscreen()),
-      ],
+      home: loggedin ? HomeScreen() : LoginPage(),
     );
   }
 }
