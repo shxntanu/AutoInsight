6de6d2cffb3b550b567d11d62837d65f0211b469
Shantanu <shantanuwable2003@gmail.com>
2023-05-13 12:15:21 +0530

    Snackbar now hides the previous one before display

--- autoinsight/lib/utils/snackbar.dart
+++ autoinsight/lib/utils/snackbar.dart
@@ -3,14 +3,17 @@ import 'colors.dart';
 
 void showSnackbar(BuildContext context, String data,
     {Duration? kDuration, Color? bgColor}) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(
-      data, 
-      style: const TextStyle(
-        color: Colors.black,
-        ),
+  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        data, 
+        style: const TextStyle(
+          color: Colors.black,
+          ),
+      ),
+      backgroundColor: bgColor ?? Colors.white,
+      duration: kDuration ?? const Duration(seconds: 3),
     ),
-    backgroundColor: bgColor ?? Colors.white,
-    duration: kDuration ?? const Duration(seconds: 3),
-  ));
+  );
 }
