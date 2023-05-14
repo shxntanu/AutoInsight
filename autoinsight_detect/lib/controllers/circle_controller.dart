d0d1498b3b5c02fca4dca2bfe5eedbd462b32772
Shantanu <shantanuwable2003@gmail.com>
2023-05-14 13:07:41 +0530

    Update to circle controller

--- autoinsight_detect/lib/controllers/circle_controller.dart
+++ autoinsight_detect/lib/controllers/circle_controller.dart
@@ -1,1 +1,21 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../controllers/user_controller.dart';
+
+final _userCtr = Get.put(userController());
+
+final _firestore = FirebaseFirestore.instance;
+const brightGreen = Color(0xFF00FF00);
+
+class circleController extends GetxController {
+  // ignore: prefer_final_fields
+  RxList _colors = [].obs;
+  RxList get getColors => _colors;
+
+  Future<void> getColorsFromDB() async {
+    final email = _userCtr.getEmail;
+    final colors = await _firestore.collection("users").doc(email).collection("coords").doc();
+  }
+}
