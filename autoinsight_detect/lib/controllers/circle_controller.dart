3dc496ca9be519fa34253ba2252ae62dd0d10276
Shantanu <shantanuwable2003@gmail.com>
2023-05-15 00:16:40 +0530

    Completed implementation of circle controller

--- autoinsight_detect/lib/controllers/circle_controller.dart
+++ autoinsight_detect/lib/controllers/circle_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -5,17 +6,54 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../controllers/user_controller.dart';
 
 final _userCtr = Get.put(userController());
+final firestore = FirebaseFirestore.instance;
+CollectionReference<Map<String, dynamic>> coords = firestore.collection("users").doc(_userCtr.getEmail).collection("coords");
 
-final _firestore = FirebaseFirestore.instance;
 const brightGreen = Color(0xFF00FF00);
 
 class circleController extends GetxController {
-  // ignore: prefer_final_fields
-  RxList _colors = [].obs;
-  RxList get getColors => _colors;
 
-  Future<void> getColorsFromDB() async {
-    final email = _userCtr.getEmail;
-    final colors = await _firestore.collection("users").doc(email).collection("coords").doc();
+  RxList<Color> colors = <Color>[
+    Colors.grey,
+    Colors.grey,
+    Colors.grey,
+    Colors.grey,
+    Colors.grey,
+    Colors.grey,
+    Colors.grey,
+    Colors.grey,
+  ].obs;
+
+  StreamSubscription<QuerySnapshot>? _coordsSubscription;
+
+  @override
+  void onInit() async {
+    await _subscribeToCoords();
+    super.onInit();
+  }
+
+  Future<void> _subscribeToCoords() async {
+    _coordsSubscription = coords.snapshots().listen(
+      (snapshot) async {
+        if (snapshot.docs.isNotEmpty) {
+          QuerySnapshot<Map<String, dynamic>> snapshot = await coords
+          .orderBy('timestamp', descending: true) // Assuming you have a 'timestamp' field in your documents
+          .limit(1)
+          .get();
+
+          final latestDoc = snapshot.docs[0].reference;
+          final data = await latestDoc.get();
+          final updatedColors = <Color>[];
+
+          for (int i = 0; i < 8; i++) {
+            final colorValue = data["$i"] == 0 ? Colors.grey : Colors.green;
+            updatedColors.add(colorValue);
+          }
+
+          colors.assignAll(updatedColors);
+          update();
+        }
+      }
+    );
   }
 }
