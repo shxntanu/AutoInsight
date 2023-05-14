9f266273f237ff58ff7846d5ae5134036eb2b8e3
Shantanu <shantanuwable2003@gmail.com>
2023-05-14 20:20:56 +0530

    Removed unused imports

--- autoinsight/lib/main.dart
+++ autoinsight/lib/main.dart
@@ -1,20 +1,11 @@
-import 'package:autoinsight/screens/navigationbarScreens/alert_screen.dart';
-import 'package:autoinsight/screens/navigationbarScreens/profile_page.dart';
 import 'package:autoinsight/screens/onboarding/login_page/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-import 'screens/unused_pages/signup_page.dart';
-import '../screens/camera_page/camera_page.dart';
-import '../screens/car_screen/car_screen.dart';
 import '../screens/dashboard/homescreen.dart';
-import '../screens/dashboard/components/newtrip.dart';
-import '../screens/videolist_screen/list_page.dart';
-import 'screens/unused_pages/start_screen.dart';
 
 import '../firebase_options.dart';
 
