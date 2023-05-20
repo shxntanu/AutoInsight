34f77678a53cefb7235768d38346ddb073936caf
Shantanu <shantanuwable2003@gmail.com>
2023-05-20 22:47:51 +0530

    Removed comments

--- autoinsight_detect/lib/controllers/backend_controller.dart
+++ autoinsight_detect/lib/controllers/backend_controller.dart
@@ -8,32 +8,6 @@ final _userCtr = Get.put(userController());
 final _firestoreInstance = FirebaseFirestore.instance;
 
 class backendController extends GetxController {
-  // Future<void> createUser(String email, String pass, String name, String phone, String address, String carName, String carYear, String licencePlate) async {
-  //   final userData = User(
-  //     email: email,
-  //     password: pass,
-  //     name: name,
-  //     phone: phone,
-  //     address: address,
-  //     carName: carName,
-  //     carYear: carYear,
-  //     licencePlate: licencePlate
-  //   );
-  //   _userCtr.setUser(userData);
-  //   await _firestoreInstance.collection("users").doc(email).collection("user_data").doc("user_info").set(userData.toMap());
-  //   await _firestoreInstance.collection("users").doc(email).collection("coords").doc("temp").set(
-  //     {
-  //       "0": 0,
-  //       "1": 0,
-  //       "2": 0,
-  //       "3": 0,
-  //       "4": 0,
-  //       "5": 0,
-  //       "6": 0,
-  //       "7": 0,
-  //     }
-  //   );
-  // }
 
   Future<dynamic> login( String email, String pass ) async {
     final user = await _firestoreInstance.collection("users").doc(email).collection("user_data").doc("user_info").get();
